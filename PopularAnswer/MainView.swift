7a333e8544614d482ec066115024dc6288e1f38c
tahadrcn <200202003@kocaeli.edu.tr>
2024-02-14 00:54:10 +0300

    Initial Commit

---
+++ PopularAnswer/MainView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  PopularAnswer
+//
+//  Created by Taha Dirican on 2.02.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView{
+        ZStack{
+            
+            Color.indigo
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                }
+            .navigationTitle("POPÜLER CEVAPLAR")
+            
+            
+            
+                
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ PopularAnswer/PopularAnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularAnswerApp.swift
+//  PopularAnswer
+//
+//  Created by Taha Dirican on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PopularAnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
