9664e6001754a46994f09e9b05599ea4c999cc0b
tahadrcn <200202003@kocaeli.edu.tr>
2024-02-23 23:54:28 +0300

    develop

--- PopularAnswer/ChooseView.swift.swift
+++ PopularAnswer/ChooseView.swift.swift
@@ -10,39 +10,56 @@ import SwiftUI
 struct ChooseView: View {
     var screenheight = UIScreen.main.bounds.height
     var screenwidth = UIScreen.main.bounds.width
+    @State var word:String = ""
+    @State var point:Int = 0
+    var Cevaplar : [String] = ["meatball","sausage","fish","cheese","doner"]
 
     var body: some View {
         ZStack{
             Color.indigo
                 .ignoresSafeArea()
             VStack{
-                Image("mainimage1").resizable()
-                    .frame(width: screenwidth*0.6, height: screenheight*0.18)
-                    .clipShape(Circle())
-                NavigationLink("Choose",destination: MainView())
+                Text("Point")
+                    .font(.title)
+                    .foregroundStyle(.ultraThickMaterial)
+                Text(String(point))
+                    .padding()
+                    .background(Color.white.opacity(0.5))
+                    .frame(width: screenwidth)
+                
+                Spacer().frame(height: screenheight*0.04)
+                
+                Text("Question")
+                    .font(.title)
+                    .foregroundStyle(.ultraThickMaterial)
+                Divider()
+                    .background(.white)
+                Spacer().frame(height: screenheight*0.1)
+                Text("........ in bread")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                
+                Spacer().frame(height: screenheight*0.15)
+                
+                
+                TextField("Write answer", text: $word)
+                    .padding()
+                    .background(Color.white.opacity(0.5))
+                    .frame(width: screenwidth*0.6)
+                
+                Button("Check",action: {
+                    if word.lowercased() == Cevaplar[0].lowercased(){
+                        point += 50
+                        word = ""
+                    }
+                })
                     .font(.title2)
                     .frame(width: screenwidth,height: screenheight*0.05)
                     .foregroundColor(.indigo)
-                    .background(Color.white)
+                    .background(Color.white.opacity(0.8))
                     .padding()
-                Image("mainimage1").resizable()
-                    .frame(width: screenwidth*0.6, height: screenheight*0.18)
-                    .clipShape(Circle())
-                NavigationLink("Choose",destination: MainView())
-                    .font(.title2)
-                    .frame(width: screenwidth,height: screenheight*0.05)
-                    .foregroundColor(.indigo)
-                    .background(Color.white)
-                    .padding()
-                Image("mainimage1").resizable()
-                    .frame(width: screenwidth*0.6, height: screenheight*0.18)
-                    .clipShape(Circle())
-                NavigationLink("Choose",destination: MainView())
-                    .font(.title2)
-                    .frame(width: screenwidth,height: screenheight*0.05)
-                    .foregroundColor(.indigo)
-                    .background(Color.white)
-                    .padding()
+                Spacer().frame(height: screenheight*0.1)
 
             }
         }
